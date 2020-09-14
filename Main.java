ad03d6b5dd68093ab0e126853ac83a353faa3763
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-14 15:58:45 +0000

    I made it calculate the slope

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,19 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter in the x coordinate of the first point");
+    int x1 =input.nextInt();
+    System.out.println("Please enter in the y coordinate of the first point");
+    int y1 =input.nextInt();
+    System.out.println("Please enter in the x coordinate of the second point");
+    int x2 =input.nextInt();
+    System.out.println("Please enter in the y coordinate of the second point");
+    int y2 =input.nextInt();
+//(y2 -y1)/(x2 - x1) 
+int slope =(y2-y1)/(x2-x1);
+System.out.println("Your slope is " + slope + ".");
+
+
   }
 }
